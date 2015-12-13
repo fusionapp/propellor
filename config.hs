22ce8f4c01ee469a4097307e72eccfcac7f8ab8e
Tristan Seligmann <mithrandi@mithrandi.net>
2015-12-13 10:48:20 +0200

    Actually install the cert.

--- config.hs
+++ config.hs
@@ -60,6 +60,7 @@ onyx = standardSystem "onyx.fusionapp.com" (Stable "jessie") "amd64"
        & File.hasPrivContent "/srv/certs/private/onyx.fusionapp.com.pem" hostContext
        & File.hasPrivContent "/srv/certs/private/sbvaf-fusion.pem" hostContext
        & File.hasPrivContent "/srv/certs/private/fusiontest.net.pem" hostContext
+       & File.hasPrivContent "/srv/certs/private/quotemaster.co.za.pem" hostContext
        & File.dirExists "/etc/docker/certs.d/scarlet.fusionapp.com:5000"
        & "/etc/docker/certs.d/scarlet.fusionapp.com:5000/ca.crt" `File.isSymlinkedTo` "/srv/certs/public/fusion-ca.crt.pem"
        & "/etc/docker/certs.d/scarlet.fusionapp.com:5000/client.cert" `File.isSymlinkedTo` "/srv/certs/private/onyx.fusionapp.com.pem"
