182cb6ad3b6dafc422848847d2d2e1474c179bbe
Tristan Seligmann <mithrandi@mithrandi.net>
2016-08-04 00:42:28 +0200

    Uh, try that again.

--- config.hs
+++ config.hs
@@ -488,11 +488,11 @@ nginxPrimary =
   & fusionSites
   & File.dirExists "/srv/www/quotemaster.co.za"
   & quotemasterSite
-  & Apt.backportsInstalled ["certbot"]
   & LetsEncrypt.letsEncrypt
   (LetsEncrypt.AgreeTOS (Just "dev@fusionapp.com"))
   "quotemaster.co.za"
   "/srv/www/quotemaster.co.za"
+  `requires` Apt.installedBackport ["certbot"]
   `onChange` Nginx.reloaded
   & saxumSite
   & saxumBrokersSite
