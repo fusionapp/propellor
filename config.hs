6a053ee981a4ab708420f2fe82173ff917c2366c
Tristan Seligmann <mithrandi@mithrandi.net>
2016-09-09 13:38:27 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -42,6 +42,7 @@ scarlet = host "scarlet.fusionapp.com" $ props
           & File.dirExists "/srv/certs/private"
           & File.hasPrivContent "/srv/certs/private/fusiontest.net-fusionca.crt.pem" hostContext
           & File.hasPrivContent "/srv/certs/private/scarlet.fusionapp.com.pem" hostContext
+          & File.hasPrivContent "/srv/certs/private/mfc-fusion-uat.pem" hostContext
           & File.dirExists "/etc/docker/certs.d/scarlet.fusionapp.com:5000"
           & "/etc/docker/certs.d/scarlet.fusionapp.com:5000/client.cert" `File.isSymlinkedTo` File.LinkTarget "/srv/certs/private/scarlet.fusionapp.com.pem"
           & "/etc/docker/certs.d/scarlet.fusionapp.com:5000/client.key" `File.isSymlinkedTo` File.LinkTarget "/srv/certs/private/scarlet.fusionapp.com.pem"
