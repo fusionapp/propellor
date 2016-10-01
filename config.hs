a83f703cabbc872fd9bdddc74727fd239822f3e6
Tristan Seligmann <mithrandi@mithrandi.net>
2016-10-01 22:46:46 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -534,7 +534,7 @@ svnSite =
   , "        proxy_set_header X-Real-IP $remote_addr;"
   , "        proxy_set_header X-Forwarded-Proto http;"
   , "        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
-  , "        proxy_pass       http://10.0.0.2:8100;"
+  , "        proxy_pass       http://127.0.0.1:8100;"
   , "    }"
   , "}"
   , ""
@@ -557,7 +557,7 @@ svnSite =
   , "        proxy_set_header X-Real-IP $remote_addr;"
   , "        proxy_set_header X-Forwarded-Proto http;"
   , "        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
-  , "        proxy_pass       http://10.0.0.2:8100;"
+  , "        proxy_pass       http://127.0.0.1:8100;"
   , "    }"
   , "}"
   ]
