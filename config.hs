2ab388fffa21dc1ffd86de654d96585196565cc2
Tristan Seligmann <mithrandi@mithrandi.net>
2018-08-13 10:23:34 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -93,7 +93,7 @@ onyx = host "onyx.fusionapp.com" $ props
 onyxDr :: Host
 onyxDr = host "onyx-dr.fusionapp.com" $ props
        & standardSystem (Stable "stretch") X86_64
-       & ipv4 "34.243.162.66"
+       & ipv4 "34.244.247.14"
        & "/etc/hostname" `File.hasContent` ["onyx-dr"]
        & cmdProperty "hostname" ["onyx-dr"] `assume` NoChange
        & fusionHost
