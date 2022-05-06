9aa8fdb8b5efa77bc3666a5e55e866bdfa64b63c
Joey Hess <joeyh@joeyh.name>
2022-05-06 13:41:55 -0400

    propellor spin

--- joeyconfig.hs
+++ joeyconfig.hs
@@ -120,7 +120,7 @@ orca = host "orca.kitenet.net" $ props
 		Testing ARM64 Nothing (Cron.Times "1 * * * *") "4h")
 
 honeybee :: Host
-honeybee = host "honeybee.lan" $ props
+honeybee = host "honeybee.kitenet.net" $ props
 	& standardSystem Testing ARMHF
 		[ "Home router and arm git-annex build box." ]
 	& Apt.removed ["rsyslog"]
