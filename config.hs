497070eb3806b204561fa9ece8d8161732ac1728
siradz <shiradz@dealersonline.co.za>
2025-09-09 13:19:14 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -113,6 +113,7 @@ onyxDr = host "onyx-dr.fusionapp.com" $ props
        & File.hasPrivContent "/srv/certs/private/prod.fusionapp.com.pem" (Context "fusion production")
        & File.hasPrivContent "/srv/certs/private/sbvaf-fusion.pem" (Context "fusion production")
        & File.hasPrivContent "/srv/certs/private/sbvaf-fusion-prod.pem" (Context "fusion production")
+       & File.hasPrivContent "/srv/certs/private/sbvaf-fusion-web-prod.pem" (Context "fusion production")
        & File.hasPrivContent "/srv/certs/private/mfc-fusion-prod.pem" (Context "fusion production")
        & File.hasPrivContent "/srv/certs/private/mfc-fusion-jwt-prod.pem" (Context "fusion production")
        & File.hasPrivContent "/srv/certs/private/fusiontest.net.pem" (Context "fusion production")
